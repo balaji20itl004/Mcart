2c1a70d1cf7535ff74babe5df31e1d57d28c49a8
balaji20itl004 <balajihari571@gmail.com>
2026-06-10 09:52:36 +0530

    enable debug

--- m_cart/settings.py
+++ m_cart/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+mo1zg4^k1c+0+&d!$unxx1r8=vdpy(zhgqo^v$)z66l*+p5ou"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
